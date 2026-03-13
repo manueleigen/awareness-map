e857c28083177221211f0cced4d988692db97d50
AeniasFritsch <mail@aeniasfritsch.de>
2026-03-13 10:17:32 +0100

    General UI styling

--- src/modules/layers.ts
+++ src/modules/layers.ts
@@ -8,6 +8,9 @@ import { renderPOILayer } from './poi.js';
 let layerDefinitions: LayerConfig[] = [];
 let context: ProjectContext | null = null;
 
+const EYE_VISIBLE = `<svg fill="currentColor" viewBox="0 0 121.025 121.025" xmlns="http://www.w3.org/2000/svg"><g><path d="M1.35,64.212c7.9,9.9,31.4,36.3,59.2,36.3c27.8,0,51.3-26.399,59.2-36.3c1.699-2.2,1.699-5.3,0-7.399c-7.9-9.9-31.4-36.3-59.2-36.3c-27.8-0.1-51.3,26.3-59.2,36.2C-0.45,58.913-0.45,62.012,1.35,64.212z M60.55,36.413c13.3,0,24,10.7,24,24s-10.7,24-24,24c-13.3,0-24-10.7-24-24S47.25,36.413,60.55,36.413z"/><circle cx="60.55" cy="60.413" r="12"/></g></svg>`;
+const EYE_HIDDEN = `<svg fill="currentColor" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg"><path d="M234.35254,160.8125a12.00024,12.00024,0,1,1-20.78516,12l-16.2771-28.19189a127.01451,127.01451,0,0,1-29.36694,13.47021l5.18994,29.43164a11.99973,11.99973,0,1,1-23.63476,4.168l-5.053-28.6543a140.4304,140.4304,0,0,1-32.94116-.01074l-5.05176,28.65234a11.99973,11.99973,0,0,1-23.63477-4.168l5.19165-29.44483A127.0154,127.0154,0,0,1,58.665,144.59326L42.28125,172.9707a12.00024,12.00024,0,0,1-20.78516-12l17.85523-30.92578A153.16947,153.16947,0,0,1,22.665,112.416,11.99959,11.99959,0,0,1,41.333,97.334C57.05859,116.79785,84.8584,140,128,140c43.14063,0,70.94043-23.20215,86.666-42.666a11.99959,11.99959,0,1,1,18.668,15.082,153.08978,153.08978,0,0,1-16.72509,17.66406Z"/></svg>`;
+
 export async function initLayers(): Promise<void> {
     try {
         const [layerData, ctxWrapper] = await Promise.all([
@@ -188,6 +191,11 @@ async function buildLayerUI(config: LayerConfig, ctxLayer: ContextLayer | null, 
             const fallbackSvg = await loadTEXT<string>('/assets/icons/default_icon.svg').catch(() => '');
             iconWrapper.innerHTML = fallbackSvg;
         }
+        const visibilityIndicator = create('span');
+        visibilityIndicator.className = 'visibility-indicator';
+        visibilityIndicator.innerHTML = isVisible ? EYE_VISIBLE : EYE_HIDDEN;
+        iconWrapper.append(visibilityIndicator);
+
         toggle.append(iconWrapper);
 
         const label = create('label');
@@ -212,6 +220,7 @@ async function buildLayerUI(config: LayerConfig, ctxLayer: ContextLayer | null, 
                 else app.activeLayers.delete(config.id);
 
                 toggle.classList.toggle('active', nowActive);
+                visibilityIndicator.innerHTML = nowActive ? EYE_VISIBLE : EYE_HIDDEN;
                 wrapper.classList.toggle('hidden', !nowActive);
                 if (sliderUI) {
                     sliderUI.classList.toggle('hidden', !nowActive);
